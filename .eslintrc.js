3d560b80e1c31528d0dfb873245add1a7b1f4d6b
Mateus oliveira <mthenriqueo@gmail.com>
2022-11-15 00:37:02 -0300

    Configure eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 module.exports = {
-  extends: ['plugin:vue/vue3-essential', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    // override/add rules settings here, such as:
-    'vue/no-unused-vars': 'error'
-  }
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'double'],
+    semi: ['error', 'always'],
+  },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   server: {
-    port: 5000
+    port: 5000,
   },
 })
